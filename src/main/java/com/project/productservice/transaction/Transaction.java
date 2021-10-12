d68196e6f3a18fbff2960a1edf751c33d40249d9
ShreyaMishra1 <shreya.mishra.guddi@gmail.com>
2021-10-12 17:30:56 +0530

    added a new file called Transaction

---
+++ src/main/java/com/project/productservice/transaction/Transaction.java
@@ -0,0 +1,19 @@
+package com.project.productservice.transaction;
+
+public class Transaction {
+	public void add(int a,int b) {
+		int sum2=a+b;
+		System.out.println("sum of two= "+sum2);
+	}
+	public void add(int a,int b,int c) {
+		int sum3=a+b+c;
+		System.out.println("sum of three= "+sum3);
+	}
+	
+	public void add(long a,long b) {
+		long sum2=a+b;
+		System.out.println("sum of long two= "+sum2);
+	}
+
+
+}
